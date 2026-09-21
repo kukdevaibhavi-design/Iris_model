7bd1f442775efeeff5b19599d269d4b1ee9875b9
Vaibhavi  kukde <kukdevaibhavi@gmail.com>
2026-09-21 11:00:30 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import stremlit as st
+import joblib
+import numpy as np
+
+#load model
+model = loblib.load('iris_model.pk')
+
+#page title
+st.title(Iris Flower Prediction App')
+
+st.header('Enter the measurement of the Iris flower:')
+
+input label
